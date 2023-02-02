377a466c23d2aa11a83d9e9c16536fce216c5fdf
Winston <98111187+WinstonHall@users.noreply.github.com>
2023-02-02 18:05:28 -0500

    Update productsRoute.ts
    Added body validation for product creation route.

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,15 +1,40 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
 import products from '../utils/products.js'
 
 
 export const productsRouter = Router()
 
-productsRouter.get('/', function(req, res) {
+productsRouter.post('/', function(req :Request, res: Response) {
 	try {
-		// const { body } = req
+		const { body } = req
+		checkBody(body)
+
+		console.log(body.product);
+		
+
 		res.status(200).json(products)
 	} catch (e) {
-		console.log(e.message)
 		res.status(400).send(e.message)
 	}
 })
+
+
+// Checks the body objec for product data
+// Filter and check required prodcut properties
+function checkBody(body){
+	if(!body.product) throw Error("Missing product data")
+	
+	const REQUIRED_PROPERTIES = ['name', 'category', 'price', 'description']
+	let errorString = ''
+	
+	// Loop through required properties for a product and concat to error string
+	REQUIRED_PROPERTIES.forEach(function(prop, index){
+		if(!body.product[prop]) 
+			(REQUIRED_PROPERTIES.length - 1 === index ) 
+			? errorString += `${prop}.` 
+			: errorString+= `${prop}, `
+	})
+	
+	// If error string has any length throw error
+	if(errorString.length) throw Error(`Missing product properties: ${errorString}`)
+}
