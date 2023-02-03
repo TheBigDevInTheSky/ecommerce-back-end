62f3d843c89d91fae95d416a186925a09a9d0286
Winston <98111187+WinstonHall@users.noreply.github.com>
2023-02-02 20:12:31 -0500

    Create Product.ts
    Created Product schema/model

---
+++ src/models/Product.ts
@@ -0,0 +1,14 @@
+import { model, Schema } from "mongoose";
+
+const Product = new Schema({
+    name:{type:String, required: true},
+    category:{type: String, required: true},
+    price:{type: Number, required: true},
+    rating: Number,
+    ratingsCount:Number,
+    thumbnails:[String],
+    images:[String],
+    description:{type: String, required: true}
+})
+
+export default model('Product', Product)
