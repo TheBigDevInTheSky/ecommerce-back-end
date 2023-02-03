db67777e97e75533282e80d9a76ac22efaded395
Winston <98111187+WinstonHall@users.noreply.github.com>
2023-02-02 20:16:05 -0500

    Removed redundant code & Added routes to get products as well as one to create a product
    Cleaned up redundant function that checked the request body.
    
    Added route handling for getting all products, and one for posting a product.

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,37 +1,24 @@
 import { Request, Response, Router } from 'express'
-import products from '../utils/products.js'
+import Product from '../models/Product.js'
 
 
 export const productsRouter = Router()
 
-productsRouter.post('/', function(req :Request, res: Response) {
+productsRouter.get('/', async function(req:Request, res:Response){
 	try {
-		const { body } = req
-		checkBody(body)		
-
-		res.status(200).json(products)
+		const products = await Product.find()
+		res.json(products)
 	} catch (e) {
 		res.status(400).send(e.message)
 	}
 })
 
-
-// Checks the body objec for product data
-// Filter and check required prodcut properties
-function checkBody(body){
-	if(!body.product) throw Error("Missing product data")
-	
-	const REQUIRED_PROPERTIES = ['name', 'category', 'price', 'description']
-	let errorString = ''
-	
-	// Loop through required properties for a product and concat to error string
-	REQUIRED_PROPERTIES.forEach(function(prop, index){
-		if(!body.product[prop]) 
-			(REQUIRED_PROPERTIES.length - 1 === index ) 
-			? errorString += `${prop}.` 
-			: errorString+= `${prop}, `
-	})
-	
-	// If error string has any length throw error
-	if(errorString.length) throw Error(`Missing product properties: ${errorString}`)
-}
+productsRouter.post('/', async function(req:Request, res:Response) {
+	try {
+		const product = await Product.create(req.body.product)
+		await product.save()
+		res.status(200).json(product)
+	} catch (e) {
+		res.status(400).send(e.message)
+	}
+})
