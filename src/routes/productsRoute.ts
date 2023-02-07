b260dc3ee8ec840edf9a088a226a7eb37210738f
Winston <98111187+WinstonHall@users.noreply.github.com>
2023-02-06 19:46:43 -0500

    Updated DB Connection
    -Implemented db disconnection after use in productsRoute.
    -Wrapped db connection in a try catch block.

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -1,10 +1,10 @@
 import { Request, Response, Router } from 'express'
 import Product from '../models/Product.js'
-
+import connectDB from '../utils/connectDB.js'
 
 export const productsRouter = Router()
 
-productsRouter.get('/', async function(req:Request, res:Response){
+productsRouter.get('/', async function (req: Request, res: Response) {
 	try {
 		const products = await Product.find()
 		res.json(products)
@@ -13,12 +13,15 @@ productsRouter.get('/', async function(req:Request, res:Response){
 	}
 })
 
-productsRouter.post('/', async function(req:Request, res:Response) {
+productsRouter.post('/', async function (req: Request, res: Response) {
+	const dbConnection = await connectDB()
 	try {
 		const product = await Product.create(req.body.product)
 		await product.save()
+		await dbConnection.disconnect()
 		res.status(200).json(product)
 	} catch (e) {
+		await dbConnection.disconnect()
 		res.status(400).send(e.message)
 	}
 })
