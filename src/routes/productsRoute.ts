605e38852d9975031aa1c9de52d3d19339abb4e0
Winston <98111187+WinstonHall@users.noreply.github.com>
2023-02-11 07:19:49 -0500

    Added route to remove product

--- src/routes/productsRoute.ts
+++ src/routes/productsRoute.ts
@@ -29,8 +29,20 @@ productsRouter.post('/', async function (req: Request, res: Response) {
 	}
 })
 
+// Find product by id, then updates the amount.
+// Takes both positive and negative numbers to adjust the amount.
 productsRouter.put('/', async function (req: Request, res: Response) {
-	// Adds to the product amount
+	const dbConnection = await connectDB()
+	try {
+		const product = await Product.findById({ _id: req.body.id })
+		product.amount += req.body.amount
+		await product.save()
+		await dbConnection.disconnect()
+		res.status(200).send()
+	} catch (e) {
+		await dbConnection.disconnect()
+		res.status(400).send(e.message)
+	}
 })
 
 productsRouter.delete('/', async function (req: Request, res: Response) {
